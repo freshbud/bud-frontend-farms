81056ba1c6f5201c98eda5c1e18ba51aad4d6d1e
BudAdmin <peaksquadentertainment@gmail.com>
2021-04-08 07:13:33 -0700

    added price charts to menu

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -38,6 +38,14 @@ const config: MenuEntry[] = [
         label: 'PancakeSwap',
         href: 'https://pancakeswap.info/token/0x6B4DA04f7662DB8abf772d973D93aF573e8aFaD5',
       },
+      {
+        label: 'Chart: AstroTools',
+        href: 'https://app.astrotools.io/pancake-pair-explorer/0x7a1aF2675513A19116ed5b523B533fBC0Ff7DCd6',
+      },
+      {
+        label: 'Chart: Poo',
+        href: 'https://poocoin.app/tokens/0x6b4da04f7662db8abf772d973d93af573e8afad5',
+      },
       // {
       //   label: 'CoinMarketCap',
       //   href: 'https://coinmarketcap.com/currencies/cub-finance/',
